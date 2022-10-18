10284d6508e620e87888d4c7a47d3f8f19f72e11
ertugrullberber <ertugrulberber3461@gmail.com>
2022-10-18 17:12:02 +0300

    Navigationitem.hidesBackButton added

--- ShowSegueAppWithCode/ViewController2.swift
+++ ShowSegueAppWithCode/ViewController2.swift
@@ -12,7 +12,7 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.navigationItem.hidesBackButton = true
     }
     
     @IBAction func geri(_ sender: Any) {
